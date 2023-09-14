7d3a8e4add44e44c77ef5e14b9be8b7daaba23d5
Hossein Salari <hosein.salari81@gmail.com>
2023-09-14 15:07:51 +0330

    updateNoteList function added

--- js/NotesView.js
+++ js/NotesView.js
@@ -1,10 +1,11 @@
 class NotesView {
   constructor(root, handlers) {
-    const { onNoteAdd, onNoteEdit } = handlers;
+    const { onNoteAdd, onNoteEdit, onNoteSelect } = handlers;
 
     this.root = root;
     this.onNoteAdd = onNoteAdd;
     this.onNoteEdit = onNoteEdit;
+    this.onNoteSelect = onNoteSelect;
 
     this.root.innerHTML = `
      <div class="notes__sidebar">
@@ -33,6 +34,49 @@ class NotesView {
         const newBody = inputBody.value.trim();
 
         this.onNoteEdit(newTitle, newBody);
+      });
+    });
+  }
+
+  #createListItemHTML(id, title, body, updated) {
+    const MAX_BODY_LENGTH = 50;
+
+    return `
+     <div class="notes__list-item" data-note-id="${id}">
+      <div class="note__small-title">${title}</div>
+      <div class="note__small-body">
+        ${body.substring(0, MAX_BODY_LENGTH)}
+        ${body.length > MAX_BODY_LENGTH ? "..." : ""}
+      </div>
+      <div class="note__small-updated">
+        ${new Date(updated).toLocaleString("en", {
+          dateStyle: "full",
+          timeStyle: "short",
+        })}
+      </div>
+     </div>
+    `;
+  }
+
+  updateNoteList(notes) {
+    const notesContainer = this.root.querySelector(".notes__list");
+
+    // empty noteList
+    notesContainer.innerHTML = "";
+
+    let notesList = "";
+
+    for (const note of notes) {
+      const { id, title, body, updated } = note;
+      const noteListItem = this.#createListItemHTML(id, title, body, updated);
+      notesList += noteListItem;
+    }
+
+    notesContainer.innerHTML = notesList;
+
+    notesContainer.querySelectorAll(".notes__list-item").forEach((noteItem) => {
+      noteItem.addEventListener("click", () => {
+        this.onNoteSelect(noteItem.dataset.noteId);
       });
     });
   }
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import NotesAPI from "./NotesAPI.js";
 import NotesView from "./NotesView.js";
 
 const app = document.getElementById("app");
+const notes = NotesAPI.getAllNotes();
 
 const view = new NotesView(app, {
   onNoteAdd() {
@@ -11,4 +12,10 @@ const view = new NotesView(app, {
   onNoteEdit(newTitle, newBody) {
     console.log(newTitle, newBody);
   },
+
+  onNoteSelect(noteId) {
+    console.log(noteId);
+  },
 });
+
+view.updateNoteList(notes);
